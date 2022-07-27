011ddd35c1378c18c7ded14be705cd46fd8f5b21
Hariomkantsharma <106576249+Hariomkantsharma@users.noreply.github.com>
2022-07-27 22:32:56 +0530

    Add files via upload
    Merge sort function added

--- file1.c
+++ file1.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+
+        int m = l + (r - l) / 2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
 void bubblesort(int size , int array[])
 {
 
