027a7016cce4b6848743dc2173d4e135b9daa17a
Hariomkantsharma <106576249+Hariomkantsharma@users.noreply.github.com>
2022-07-27 22:35:27 +0530

    Add files via upload
    Quick sort function added

--- file1.c
+++ file1.c
@@ -1,5 +1,90 @@
 #include<stdio.h>
 
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int array[], int low, int high)
+{
+    int pivot = array[high];
+    int i = (low - 1);
+
+    for (int j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }    
+    swap(&array[i + 1], &array[high]);
+
+    return (i + 1);
+}
+
+void quickSort(int array[], int low, int high)
+{
+    if (low < high)
+    {
+
+        int pi = partition(array, low, high);
+        quickSort(array, low, pi - 1);
+        quickSort(array, pi + 1, high);
+    }
+}
+
+
+void merge(int arr[], int p, int q, int r)
+{
+
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    int L[n1], M[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[p + i];
+    for (int j = 0; j < n2; j++)
+        M[j] = arr[q + 1 + j];
+
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = p;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= M[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = M[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = M[j];
+        j++;
+        k++;
+    }
+}
+
 void mergeSort(int arr[], int l, int r)
 {
     if (l < r)
