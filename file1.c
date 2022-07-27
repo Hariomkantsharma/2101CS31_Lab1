f2f0e735889b97961f41c8dad58a1e9345f71f46
Hariomkantsharma <106576249+Hariomkantsharma@users.noreply.github.com>
2022-07-27 22:31:42 +0530

    Add files via upload
    Bubble sort function added

--- file1.c
+++ file1.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
+void bubblesort(int size , int array[])
+{
+
+    for (int step = 0; step < size - 1; ++step)
+    {
+
+        for (int i = 0; i < size - step - 1; ++i)
+        {
+
+            if (array[i] > array[i + 1])
+            {
+                int temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+        }
+    }
+}
+
+
 void select_sort(int size  , int array[])
 {
     for (int step = 0; step < size - 1; step++)
